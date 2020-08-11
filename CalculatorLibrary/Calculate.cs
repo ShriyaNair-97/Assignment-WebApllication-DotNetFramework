820851a44c8aab602da76bb8669e860c963128ba
ShriyaNair-97 <68274640+ShriyaNair-97@users.noreply.github.com>
2020-08-11 19:29:13 +0530

    Add files to repository

---
+++ CalculatorLibrary/Calculate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CalculatorLibrary
+
+{
+    
+    public class Calculate
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+        public int Substract()
+        {
+            if (FirstNumber > SecondNumber)
+            {
+                return FirstNumber - SecondNumber;
+            }
+            else
+            {
+                return SecondNumber - FirstNumber;
+            }
+
+        }
+        public int Multiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+        public double Divide()
+        {
+            return FirstNumber / SecondNumber;
+        }
+    }
+}
