4e0f59c305d0f8a387a2ad7303d51203d8d5ed55
ShriyaNair-97 <68274640+ShriyaNair-97@users.noreply.github.com>
2020-08-11 19:31:05 +0530

    Add files to repository

---
+++ ConCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using CalculatorLibrary;
+
+namespace ConCalculator
+{
+    [Serializable]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal = new Calculate();
+            FileStream fs = new FileStream("Calculator.txt" ,
+                FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs,cal);
+            Console.WriteLine("Serialization done");
+            fs.Close();
+
+        }
+}
+}
